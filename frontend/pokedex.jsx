e69e3dfeaeb6e022d09de8bb532afc22c12d6164
ashap94 <aaronshapiro94@gmail.com>
2020-03-11 15:49:07 -0700

    Added thunk middleware to allow for asynchronous action creators to fetch data from server and modify upon receiving payload of data

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { receiveAllPokemon } from "./actions/pokemon_actions";
+import {
+  receiveAllPokemon,
+  requestAllPokemon
+} from "./actions/pokemon_actions";
 import { fetchAllPokemon } from "./util/api_util";
 import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
   window.receiveAllPokemon = receiveAllPokemon;
   window.fetchAllPokemon = fetchAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
 
   const store = configureStore();
   window.getState = store.getState;
