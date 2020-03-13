1f8ef78f69f6e3eda336fcd993beb7e88335d2a9
ashap94 <aaronshapiro94@gmail.com>
2020-03-12 17:39:52 -0700

    Fixed issue of ui reducer not returning default state under non usage

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,3 +1,11 @@
-const uiReducer = () => {};
+const uiReducer = (state = {}, action) => {
+  Object.freeze(state);
+  let newState = {};
+
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
 
 export default uiReducer;
